f1ea69bf0ce2f39d0502f4f49aae247fdffe405f
ucbtns <38843567+ucbtns@users.noreply.github.com>
2020-01-29 17:50:48 +0000

    main_file

--- src/main.py
+++ src/main.py
@@ -8,39 +8,40 @@ Stochastic environment
 import os
 os.chdir('D:/PhD/Code/bayes')
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import stochastic as st
 import deterministic as dt
 import utils as ut
 
 odd = list(range(20)) + list(range(120,140)) + list(range(250,450))
-simulator = False
-det = False
+simulator = True
+det = True
 sto = True
 
+num = ''
 if simulator:
     if sto:
-        st.stochastic(odd, '1703')
+        st.stochastic(odd, num)
     if det:
-        dt.deterministic('1703')
+        dt.deterministic(num)
 
 
 if sto:
     name = 'stochastic'
-    br = 'brl_tr_online.npy'
-    ql = 'ql_tr_online.npy'
-    ql_no = 'ql_tr_online_no.npy'
+    br = 'brl_tr_online' + num + '.npy'
+    ql = 'ql_tr_online' + num + '.npy'
+    ql_no = 'ql_tr_online_no' + num + '.npy'
     ai = 'trwp.csv'
     ai_no = 'trwop.csv'
 if det:
     name = 'deterministic'
-    br = 'brl_tr_det_online.npy'
-    ql = 'ql_tr_det_online.npy'
-    ql_no = 'ql_tr_det_online_no.npy'
+    br = 'brl_tr_det_online' + num + '.npy'
+    ql = 'ql_tr_det_online' + num + '.npy'
+    ql_no = 'ql_tr_det_online_no' + num + '.npy'
     ai = 'trwp_det.csv'
-    ai_no = 'trwop_Det.csv'
-   
+    ai_no = 'trwop_det.csv'
+    
+    
 
 BRL_tr_online = np.load(br) 
 QL_tr_online = np.load(ql) 
@@ -64,6 +65,9 @@ trwop = np.mean(trwop_rewards, axis = 1)
 ci[4,0], ci[4,1], ci[4,2] =ut.mean_confidence_interval(trwop)
 
 
-ut.plot_subresults(name, qlno, ql, brl, trwp, trwop, sto)
-
+if det:
+    ut.plot_subresultsci('deterministic_plot', QL_tr_online_no, QL_tr_online, BRL_tr_online, trwp_rewards, trwop_rewards, 500, False)
+if sto:
+    ut.plot_subresultsci('stochastic_plot', QL_tr_online_no, QL_tr_online, BRL_tr_online, trwp_rewards, trwop_rewards, 500, True)
+    
 
